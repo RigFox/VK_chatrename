3c78580f2ba396f3540c027aa9df538d13252d45
RigFox <rigfox@gmail.com>
2015-09-19 23:28:03 +0500

    Add rename functional

---
+++ setting/setting.example.py
@@ -0,0 +1,6 @@
+# Переименовать в setting.py
+
+__doc__ = "Сливные настройки"
+
+VK_API_TOKEN = "BLABLABLA_SECRET"  # ВК токен
+VK_CHAT_ID = -1
---
+++ src/sliv.py
@@ -0,0 +1,24 @@
+from setting.setting import VK_API_TOKEN
+from setting.setting import VK_CHAT_ID
+import requests
+import datetime
+import logging
+
+logging.basicConfig(format=u'%(levelname)-8s [%(asctime)s] %(message)s',
+                    level=logging.DEBUG,
+                    filename=u'../log/log.log')
+
+fSeptember = "01.09.2015"
+fSeptemberDate = datetime.datetime.strptime(fSeptember, "%d.%m.%Y")
+
+nowDate = datetime.datetime.now()
+
+diffDate = nowDate - fSeptemberDate
+
+pmProgress = str(round(2 + (diffDate.days / 365), 6)) + " ПМиИ"
+
+payload = {"chat_id": VK_CHAT_ID, "title": pmProgress, "access_token": VK_API_TOKEN+"b"}
+r = requests.get('https://api.vk.com/method/messages.editChat', params=payload)
+
+logging.debug("VK API return: " + r.text)
+
