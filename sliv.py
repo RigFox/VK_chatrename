4b0635aaf541e61fb0d564ac15d33d80621da877
RigFox <rigfox@gmail.com>
2015-09-20 11:24:43 +0500

    Add coding directive

--- sliv.py
+++ sliv.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from setting.setting import VK_API_TOKEN
 from setting.setting import VK_CHAT_ID
 import requests
@@ -6,6 +8,7 @@ import logging
 
 logging.basicConfig(format=u'%(levelname)-8s [%(asctime)s] %(message)s',
                     level=logging.DEBUG,
+                    encoding='utf-8',
                     filename=u"log/log.log")
 
 fSeptember = "01.09.2015"
@@ -17,8 +20,7 @@ diffDate = nowDate - fSeptemberDate
 
 pmProgress = str(round(2 + (diffDate.days / 365), 6)) + " ПМиИ"
 
-payload = {"chat_id": VK_CHAT_ID, "title": pmProgress, "access_token": VK_API_TOKEN}
+payload = {"chat_id": VK_CHAT_ID, "title": pmProgress, "access_token": VK_API_TOKEN + "b"}
 r = requests.get('https://api.vk.com/method/messages.editChat', params=payload, timeout=10)
 
 logging.debug("VK API return: " + r.text)
-
