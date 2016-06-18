6d5961628cdfa1b54e2290edc73e112ee5ca236f
Victor Plis <rigfox@gmail.com>
2016-06-18 12:32:33 +0600

    Update sliv.py
    Added correct declination

--- sliv.py
+++ sliv.py
@@ -25,7 +25,15 @@ nowDate = datetime.datetime.now()
 
 diffDate = fSeptemberDate - nowDate
 
-pmProgress = str(round(2 + (92 / diffDate.days), 6)) + " ПМиИ. " + str(diffDate.days) + " дней до 1 сентября"
+dayStr = "день"
+if diffDate.days // 10 == 1:
+  dayStr = "дней"
+elif (diffDate.days % 10 == 0) or (diffDate.days % 10 >= 5):
+  dayStr = "дней"
+elif (diffDate.days % 10 in [2,3,4]):
+  dayStr = "дня"
+
+pmProgress = str(round(2 + (92 / (92 - diffDate.days)), 6)) + " ПМиИ. " + str(diffDate.days) + " дней до 1 сентября"
 
 payload = {"chat_id": VK_CHAT_ID, "title": pmProgress, "access_token": VK_API_TOKEN}
 r = requests.get('https://api.vk.com/method/messages.editChat', params=payload, timeout=10)
